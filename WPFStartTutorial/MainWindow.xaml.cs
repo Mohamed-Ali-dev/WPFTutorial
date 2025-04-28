03b1bc28593600438ba740ee16cc574c90d79f08
Flothers1 <ifreeze@flothers.com>
2025-04-28 21:52:13 +0300

    first commit

---
+++ WPFStartTutorial/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace WPFStartTutorial
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+        private string boundText;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string BoundText
+        {
+            get { return boundText; }
+            set 
+             {
+                //whenever this setter is set invoke the event that this property(BoundText) has changed
+
+                boundText = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BoundText"));
+            }
+        }
+
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "set from code";
+        }
+    }
+} 
