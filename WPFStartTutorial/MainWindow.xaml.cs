ac383d7ee2f033089f58b4fd25a9f8e258f13aee
Flothers1 <ifreeze@flothers.com>
2025-05-04 23:59:18 +0300

    Open file dialog

--- WPFStartTutorial/MainWindow.xaml.cs
+++ WPFStartTutorial/MainWindow.xaml.cs
@@ -1,7 +1,5 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿using Microsoft.Win32;
 using System.Windows;
-using System.Windows.Navigation;
 
 namespace WPFStartTutorial
 {
@@ -14,17 +12,33 @@ namespace WPFStartTutorial
 
         private void btnFire_Click( object sender, RoutedEventArgs e)
         {
-            //MessageBox.Show("Your message here.","ERROR!", MessageBoxButton.OK, MessageBoxImage.Error);
-            MessageBoxResult result = MessageBox.Show("Do you agree?", "Agreement.",
-                MessageBoxButton.YesNo, MessageBoxImage.Question);
-            if (result == MessageBoxResult.Yes)
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            //Select just the .pdf files
+            fileDialog.Filter = "pdf Source Files | *.pdf";
+            //specify initial directory to open. 
+            //if the path is not found it will open the default directory 
+            fileDialog.InitialDirectory = "D:\\Projects\\WPF\\WPFStartTutorial";
+            //To add a title for the dialog 
+            fileDialog.Title = "Please select  pdf file(s)";
+            //Allow multi selection
+            fileDialog.Multiselect = true;
+
+            bool? success = fileDialog.ShowDialog();
+            if(success == true)
             {
-                tblInfo.Text = "Agreed";
+                string[] paths = fileDialog.FileNames;
+                //The file name with no path
+                string[] fileNames = fileDialog.SafeFileNames;
+                foreach (var fileName in fileNames)
+                {
+                    tblInfo.Text = fileName;
+
+                }
             }
             else
             {
-                tblInfo.Text = "Not Agreed";
+
             }
         }
     }
-} 
+}  
